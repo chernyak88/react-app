4f81615e9b8c169e6b2eb4551456200639040b93
Igor Chernyak <i_chernyak@inbox.ru>
2020-09-07 15:44:03 +0300

    Updates

---
+++ precache-manifest.e0d2e547b65330833744080e08a194a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ac6542cf1549b10689a03223fb4edf",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "62704367972ce258f5bc",
+    "url": "/react-app/static/css/main.bd213ed8.chunk.css"
+  },
+  {
+    "revision": "fc273f63ce1c1b44802b",
+    "url": "/react-app/static/js/2.11f55dfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.11f55dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62704367972ce258f5bc",
+    "url": "/react-app/static/js/main.02bfcd64.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  }
+]);
